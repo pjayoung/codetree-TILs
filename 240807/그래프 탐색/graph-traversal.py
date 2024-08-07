8a3c4e5c88cf67a2069085991b4bbd0770b1c9d3
JAYOUNG PARK <151982532+pjayoung@users.noreply.github.com>
2024-08-07 15:15:09 +0900

    Title: 그래프 탐색, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240807/그래프 탐색/graph-traversal.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+visited = [False for _ in range(n+1)]
+
+start_points = []
+end_points = []
+for _ in range(m):
+    s, e = map(int, input().split())
+    start_points.append(s)
+    end_points.append(e)
+
+for start, end in zip(start_points, end_points):
+    graph[start].append(end)
+    graph[end].append(start)
+
+cnt = 0
+def DFS(vertex):
+    global cnt
+    for curr_v in graph[vertex]:
+        if not visited[curr_v]:
+            visited[curr_v] = True
+            cnt += 1
+            DFS(curr_v)
+    return cnt
+
+root_vertex = start_points[0]
+visited[root_vertex] = True
+print(DFS(root_vertex))
