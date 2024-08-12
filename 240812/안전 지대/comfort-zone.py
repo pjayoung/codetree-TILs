11bad763a14c1aa37cb2b2d4daebb7bdf4ff6cb7
JAYOUNG PARK <151982532+pjayoung@users.noreply.github.com>
2024-08-12 17:40:11 +0900

    Title: 안전 지대, Time: 1968ms, Memory: 165MB, Status: Passed - CodeTree

--- 240812/안전 지대/comfort-zone.py
+++ 240812/안전 지대/comfort-zone.py
@@ -2,46 +2,82 @@ import sys
 
 sys.setrecursionlimit(2500)
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+n, m = tuple(map(int, input().split()))
+grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(n)]
 
-# 상하좌우 이동을 위한 방향 벡터
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+zone_num = 0
 
-def dfs(x, y, K, visited):
-    visited[x][y] = True
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and graph[nx][ny] > K:
-            dfs(nx, ny, K, visited)
 
-def count_safe_zones(K):
-    visited = [[False] * m for _ in range(n)]
-    safe_zone_count = 0
-
+# visited 배열을 초기화해줍니다.
+def initialize_visited():
     for i in range(n):
         for j in range(m):
-            if graph[i][j] > K and not visited[i][j]:
-                dfs(i, j, K, visited)
-                safe_zone_count += 1
+            visited[i][j] = False
 
-    return safe_zone_count
 
-def find_max_safe_zones():
-    max_height = max(max(row) for row in graph)
-    max_safe_zones = 0
-    optimal_K = 0
+# 주어진 위치가 격자를 벗어나는지 여부를 반환합니다.
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < m
 
-    for K in range(1, max_height + 1):
-        safe_zone_count = count_safe_zones(K)
-        if safe_zone_count > max_safe_zones:
-            max_safe_zones = safe_zone_count
-            optimal_K = K
 
-    return optimal_K, max_safe_zones
+# 주어진 위치로 이동할 수 있는지 여부를 확인합니다.
+def can_go(x, y, k):
+    if not in_range(x, y):
+        return False
+    
+    if visited[x][y] or grid[x][y] <= k:
+        return False
+    
+    return True
 
-# 결과 출력
-optimal_K, max_safe_zones = find_max_safe_zones()
-print(optimal_K, max_safe_zones)
+
+def dfs(x, y, k):
+    # 0: 오른쪽, 1: 아래쪽, 2: 왼쪽, 3: 위쪽
+    # dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+    dxs, dys = [-1,1,0,0], [0,0,-1,1]
+
+    # 네 방향에 각각에 대하여 DFS 탐색을 합니다.
+    for dx, dy in zip(dxs, dys):
+        new_x, new_y = x + dx, y + dy
+        
+        if can_go(new_x, new_y, k):
+            visited[new_x][new_y] = True
+            dfs(new_x, new_y, k)
+
+
+def get_zone_num(k):
+    global zone_num
+    
+    # 새로운 탐색을 시작한다는 의미로 zone_num를 0으로 갱신하고 
+    # visited 배열을 초기화해줍니다.
+    zone_num = 0
+    initialize_visited()
+    
+    # 격자의 각 위치에 대하여 탐색을 시작할 수 있는 경우
+    # 해당 위치로부터 시작한 DFS 탐색을 수행합니다.
+    for i in range(n):
+        for j in range(m):
+            if can_go(i, j, k):
+                # 해당 위치를 탐색할 수 있는 경우 visited 배열을 갱신하고
+                # 안전 영역을 하나 추가해줍니다.
+                visited[i][j] = True
+                zone_num += 1
+                
+                dfs(i, j, k)
+
+
+# 가능한 안전 영역의 최솟값이 0이므로 다음과 같이 초기화 해줄 수 있습니다.
+max_zone_num = -1
+answer_k = 0
+max_height = 100
+
+# 각 가능한 비의 높이에 대하여 안전 영역의 수를 탐색합니다.
+for k in range(1, max_height +1):
+    get_zone_num(k)
+    
+    # 기존의 최대 영역의 수보다 클 경우 이를 갱신하고 인덱스를 저장합니다.
+    if zone_num > max_zone_num:
+        max_zone_num, answer_k = zone_num, k
+
+print(answer_k, max_zone_num)
